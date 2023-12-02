c6fcd3f03376514cf1830e7a8e98186f76ac9109
MADIHAH BINTI MOHD RADZUAN <152683431+madihahradzuan@users.noreply.github.com>
2023-12-02 17:52:02 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+//script.js 
+
+<script>
+var sidemenu = document.getElementsById("sidemenu");
+function openmenu(){
+	sidemenu.style.right="0";
+	}
+function closemenu(){
+	sidemenu.style.right="-200px";
+	}
+}
+</script>
+
